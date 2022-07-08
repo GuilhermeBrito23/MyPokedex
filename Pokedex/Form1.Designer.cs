40f28b53d007a93cbebd846a60c953eb85928b7a
GuilhermeBrito23 <guilherme.brito@disroot.org>
2022-07-08 16:37:33 -0300

    Pokedex com Windows Forms

---
+++ Pokedex/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Pokedex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBoxPokemon = new System.Windows.Forms.PictureBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPokemon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxPokemon
+            // 
+            this.picBoxPokemon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picBoxPokemon.Location = new System.Drawing.Point(301, 59);
+            this.picBoxPokemon.Name = "picBoxPokemon";
+            this.picBoxPokemon.Size = new System.Drawing.Size(170, 151);
+            this.picBoxPokemon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picBoxPokemon.TabIndex = 0;
+            this.picBoxPokemon.TabStop = false;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(114, 315);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisar.TabIndex = 1;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(114, 212);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 20);
+            this.txtNum.TabIndex = 2;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(114, 258);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(331, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Numero :";
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(397, 237);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(25, 13);
+            this.lblNum.TabIndex = 5;
+            this.lblNum.Text = "Null";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(397, 276);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(25, 13);
+            this.lblNome.TabIndex = 7;
+            this.lblNome.Text = "Null";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(331, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nome :";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(397, 315);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(25, 13);
+            this.lblTipo.TabIndex = 9;
+            this.lblTipo.Text = "Null";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(331, 315);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Tipo :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Nome :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Numero :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblNum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.picBoxPokemon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPokemon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxPokemon;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Pokedex/Form1.cs
@@ -0,0 +1,72 @@
+﻿using QuickType;
+using System;
+using System.Net.Http;
+using System.Windows.Forms;
+namespace Pokedex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            string apiUrl ="";
+            if (String.IsNullOrEmpty(txtNome.Text)!=true && String.IsNullOrEmpty(txtNum.Text)!= true)
+            {
+                MessageBox.Show("Por gentileza preencher apenas um campo");
+                return;
+            }
+            else if (String.IsNullOrEmpty(txtNome.Text) != true && String.IsNullOrEmpty(txtNum.Text) == true)
+            {
+
+                 apiUrl = $"https://pokeapi.co/api/v2/pokemon/{txtNome.Text.ToLower()}";
+
+            }else if (String.IsNullOrEmpty(txtNome.Text) == true && String.IsNullOrEmpty(txtNum.Text) != true)
+            {
+                 apiUrl = $"https://pokeapi.co/api/v2/pokemon/{txtNum.Text.ToLower()}";
+            }
+            else
+            {
+                MessageBox.Show("Campos Vazio!");
+                return;
+            }
+
+
+            using (HttpClient client = new HttpClient())
+            {
+                var apiResponse = client.GetAsync(apiUrl).Result;
+                if (apiResponse.IsSuccessStatusCode)
+                {
+                    var result = apiResponse.Content.ReadAsStringAsync().Result;
+
+
+                    var pokemon = Pokemon.FromJson(result);
+                    lblNum.Text = pokemon.Id.ToString();
+                    lblNome.Text = pokemon.Name;
+                    string types = "";
+                    for (int i = 0; i < pokemon.Types.Length; i++)
+                    {
+                        if (pokemon.Types.Length > 1)
+                        {
+                            types += "/" + pokemon.Types[i].Type.Name.ToString();
+                        }
+                        else
+                        {
+                            types += pokemon.Types[i].Type.Name.ToString();
+                        }
+
+                    }
+                    lblTipo.Text = types;
+
+                    picBoxPokemon.ImageLocation = pokemon.Sprites.FrontDefault.AbsoluteUri;
+                }
+            }
+
+
+        }
+    }
+}
